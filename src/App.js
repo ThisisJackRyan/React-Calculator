6277cd78de6763927add5d019985a6058b3bd25c
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-03-29 18:20:31 -0500

    loop a componet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import CalcButtons from './CalculatorButtons';
+import _ from 'lodash';
+
 
 function App() {
+  let buttons = [];
+    _.times(8, (i) => {
+      buttons.push(<CalcButtons num={i} />);
+    });
   return (
     <div className="App">
-     <div></div>
+      <div>Output</div>
+      <div>
+        <div>{buttons}</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/CalculatorButtons.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CalculatorButtons = ({num}) => {
+    const log = (i) =>{
+        console.log(i + " This is the number you clicked.")
+    }
+    return (
+        <div>
+            <button onClick={log(num)}>{num}</button>
+        </div>
+    )
+}
+
+export default CalculatorButtons;
